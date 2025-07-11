fc6333e50686e6a593d434c9610df2a070fa8d77
asebrech <alois.sebrechts@gmail.com>
2025-07-11 21:59:56 +0200

    rm useless imports

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,4 @@
-use interface::shell_game::new_game;
-
-mod ai {
-    pub mod heuristic;
-    pub mod minimax;
-    pub mod transposition;
-}
-mod core {
-    pub mod board;
-    pub mod captures;
-    pub mod moves;
-    pub mod rules;
-    pub mod state;
-}
-mod interface {
-    pub mod shell_game;
-    pub mod utils;
-}
+use gomoku::interface::shell_game::new_game;
 
 fn main() {
     new_game(8, 4, 4);
